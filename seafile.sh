e53af39f56e052834fbfb3d15f1b6aef781d3fae
Greg Myers <greg@gregmyers.net>
2018-03-19 14:45:51 -0700

    / ‘seafile.sh’

--- seafile.sh
+++ seafile.sh
@@ -3,4 +3,12 @@ set -ex
 
 adduser greg
 usermod -aG sudo greg
+# vi /etc/ssh/sshd_config
+apt update
+apt upgrade -y
 
+su sammy
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+nano ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
